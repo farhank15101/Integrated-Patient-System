065c5bcbdafaea1705e85f8dd194f7417f142af7
farhank15101 <105463415+farhank15101@users.noreply.github.com>
2022-05-15 00:20:20 -0400

    Add files via upload

---
+++ Patient Project/src/com/med/MedCond.java
@@ -0,0 +1,53 @@
+package com.med;
+
+public class MedCond {
+    String mdContact;
+    String mdPhone;
+    String algType;
+    String illType;
+
+    public MedCond(String mdContact,String mdPhone,String algType,String illType){
+        this.mdContact=mdContact;
+        this.mdPhone=mdPhone;
+        this.algType=algType;
+        this.illType=illType;
+
+    }
+
+    public String getMdContact(){
+        return this.mdContact;
+    }
+
+    public String getMdPhone(){
+        return this.mdPhone;
+    }
+    public String getAlgType(){
+        return this.algType;
+    }
+
+    public String getIllType(){
+        return this.illType;
+    }
+
+    public void updateMdContact(String mdContact){
+        this.mdContact=mdContact;
+    }
+
+    public void updateMdPhone(String mdPhone){
+        this.mdPhone=mdPhone;
+    }
+
+    public void updateAlgType(String algType){
+        this.algType=algType;
+    }
+
+    public void updateIllType(String illType){
+        this.illType=illType;
+    }
+
+
+
+
+
+}
+
